0965737d449067385c33b4436202a302e8793a85
Rahul Varun Kotagiri <rahulvarun9441@gmail.com>
2026-04-22 23:46:05 +0530

    Renamed Chapter-4 folder

---
+++ chapter-3(Data Typs)/script.js
@@ -0,0 +1,15 @@
+console.log("I am good boy");
+
+console.log("I have master's degree");
+
+console.log('I am the "best".');
+
+console.log("I am the ", "asfdsadfa", 1000, "adsfda");
+
+console.log("I am\nlearning\nJavaScript\nfrom\ndocumentation");
+
+console.log(`I am
+learning
+JavaScript
+from
+documentation`);
