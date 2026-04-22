76758cfd0b1554c4ca80c969aa9b749fc3ef7c2c
Rahul Varun Kotagiri <rahulvarun9441@gmail.com>
2026-04-22 23:32:12 +0530

    Adding chapter-2

---
+++ chapter-2(Alert command)/script.js
@@ -0,0 +1,15 @@
+console.log("welcome");
+alert("welcome to the alert command chapter");
+window.alert("welcome to the alert command chapter");
+
+//we can also use alert without window as it is the global object in browser environment
+
+// Here window is the global object and alert is a method of the window object. So we can call it directly without using window.
+
+console.log(window);
+// In the above code, we are logging the window object to the console. The window object represents the browser window and provides various properties and methods to interact with it.
+
+
+console.log(this);
+
+//windows==this: botha are global variables
